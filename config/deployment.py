9594fbeb6e781de53c62adcabb083ee59d0a1b32
Gustavo <gaberdugov@unal.edu.co>
2024-12-01 17:53:02 -0500

    Cambios en todo

--- config/deployment.py
+++ config/deployment.py
@@ -10,8 +10,8 @@ SECRET_KEY = os.environ['MY_SECRET_KEY']
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -36,7 +36,7 @@ CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.
 
 DATABASES = {
     "default": {
-        "ENIGNE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": CONNECTION_STR['dbname'],
         "HOST": CONNECTION_STR['host'],
         "USER": CONNECTION_STR['user'],
